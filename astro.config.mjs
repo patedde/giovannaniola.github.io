383589d17c417a28c671dd53b02f9c8acc922749
patedde <122355697+patedde@users.noreply.github.com>
2025-11-26 10:56:36 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tafano.github.io',
-  base: '/niolaterapia.com',
+  site: 'https://niolaterapia.com',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
