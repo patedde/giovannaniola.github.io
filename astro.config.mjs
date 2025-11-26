2b2ea7466789a177362934ee7751ec6240cf2cc7
patedde <122355697+patedde@users.noreply.github.com>
2025-11-26 11:30:34 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import staticAdapter from '@astrojs/adapter-static'; 
 
 export default defineConfig({
   site: 'https://niolaterapia.com',
   output: 'static',
-  adapter: staticAdapter(),
   integrations: [tailwind()],
 });
