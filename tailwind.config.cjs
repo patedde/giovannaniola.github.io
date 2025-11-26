7c442a73e876e9098bc7af3786c19680224a85ec
patedde <122355697+patedde@users.noreply.github.com>
2025-11-26 11:23:43 +0000

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        serif: ['Lora', 'ui-serif', 'Georgia', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
