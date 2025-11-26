c5673558a617515d1c9cded70eae3782190ea57f
patedde <122355697+patedde@users.noreply.github.com>
2025-11-26 11:16:48 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
+import staticAdapter from '@astrojs/static'; 
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://terapianiola.com',
-  // Remove base entirely for custom domain
   output: 'static',
   adapter: staticAdapter(),
   integrations: [tailwind()],
